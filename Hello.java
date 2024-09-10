fe6b924ea55ea8afbcd5598b6c3291f7beea4c07
Lui-Lop <lopezlui@grinnell.edu>
2024-09-09 19:29:30 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ public class Hello {
     public static void main (String[] args) {
       java.io.PrintWriter pen;
     pen = new java.io.PrintWriter(System.out, true);
-    pen.println ("Hello, GitHub Again Again?");
+    pen.println ("Hello, GitHub Again!");
+    pen.println ("Hey");
     pen.flush();
     pen.close();
   } // main(String[])
